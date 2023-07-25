a04ed8061a62c1bf2e3b4f83a61c60f70baee1eb
Arthi Sivakumar <arthisivakumar19@gmail.com>
2023-07-25 20:04:48 +0200

    Changes made in Newyear.html file

---
+++ Arthi/Newyear_Countdown/firework.js
@@ -0,0 +1,27 @@
+(() => {
+
+    const canvas = document.getElementById('firework');
+
+    const context = canvas.getContext('2d');
+
+
+
+
+    const width = window.innerWidth;
+
+    const height = window.innerHeight;
+
+
+
+
+    const positions = {
+
+        mouseX: 0,
+
+        mouseY: 0,
+
+        wandX: 0,
+
+        wandY: 0
+
+    };
